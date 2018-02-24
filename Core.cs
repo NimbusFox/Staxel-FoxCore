8b4e5ee5364edfd0e534b842218c9338b595e645
NimbusFox <simonbailey10@hotmail.com>
2018-02-24 15:29:47 +0000

    Dependency Resolver
    Worked on the Resolver which will now load a class if it exists

--- Core.cs
+++ Core.cs
@@ -28,21 +28,22 @@ namespace NimbusFox.FoxCore {
         }
 
         public static T ResolveOptionalDependency<T>(string key) {
-            var dir = Assembly.GetExecutingAssembly().Location;
+            var assembly = Assembly.GetAssembly(typeof(FxCore));
+            var dir = assembly.Location.Substring(0, assembly.Location.LastIndexOf("\\", StringComparison.Ordinal));
+            Console.WriteLine(dir);
             foreach (var file in new DirectoryInfo(dir).GetFiles("*.mod")) {
+                Console.WriteLine(file.FullName);
                 var data = JsonConvert.DeserializeObject<Dictionary<string, object>>(File.ReadAllText(file.FullName));
 
                 if (data.Any(x => x.Key.ToLower() == "fxdependencykeys")) {
-                    var current = data[data.First(x => x.Key.ToLower() == "fxdependencykeys").Key];
-
-                    if (current is string[] keys) {
-                        if (keys.Any()) {
-                            if (keys.Any(x => string.Equals(x, key, StringComparison.CurrentCultureIgnoreCase))) {
-                                var item = Assembly.LoadFile(file.FullName.Replace(".mod", ".dll"));
-                                foreach (var module in item.GetModules()) {
-                                    if (module.GetType() is T) {
-                                        return (T)Activator.CreateInstance(module.GetType());
-                                    }
+                    var current = JsonConvert.DeserializeObject<string[]>(JsonConvert.SerializeObject(data[data.First(x => x.Key.ToLower() == "fxdependencykeys").Key]));
+                    if (current.Any()) {
+                        if (current.Any(x => string.Equals(x, key, StringComparison.CurrentCultureIgnoreCase))) {
+                            var item = Assembly.LoadFile(file.FullName.Replace(".mod", ".dll"));
+                            foreach (var module in item.DefinedTypes) {
+                                if (module.ReflectedType != null && module.ReflectedType.GetInterfaces().Contains(typeof(T))) {
+                                    Console.WriteLine("FOUND ONE");
+                                    return (T)Activator.CreateInstance(module.ReflectedType);
                                 }
                             }
                         }
