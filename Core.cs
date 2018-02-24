ad8b0e0b3156dd4b0385f8d71d7051e6ef5911b4
NimbusFox <simonbailey10@hotmail.com>
2018-02-24 01:13:23 +0000

    Dependency system
    Introducing dependency system to allow mods to call upon interface
    based mods to allow communication between mods without being directly
    dependent on a mod

--- Core.cs
+++ Core.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Newtonsoft.Json;
 using NimbusFox.FoxCore.Managers;
 using NimbusFox.FoxCore.Managers.Particles;
 using Staxel.FoxCore.Managers.Particles;
@@ -20,6 +25,32 @@ namespace NimbusFox.FoxCore {
             ParticleManager = new ParticleManager();
             EntityParticleManager = new EntityParticleManager();
             EntityFollowParticleManager = new EntityFollowParticleManager();
+        }
+
+        public static T ResolveOptionalDependency<T>(string key) {
+            var dir = Assembly.GetExecutingAssembly().Location;
+            foreach (var file in new DirectoryInfo(dir).GetFiles("*.mod")) {
+                var data = JsonConvert.DeserializeObject<Dictionary<string, object>>(File.ReadAllText(file.FullName));
+
+                if (data.Any(x => x.Key.ToLower() == "fxdependencykeys")) {
+                    var current = data[data.First(x => x.Key.ToLower() == "fxdependencykeys").Key];
+
+                    if (current is string[] keys) {
+                        if (keys.Any()) {
+                            if (keys.Any(x => string.Equals(x, key, StringComparison.CurrentCultureIgnoreCase))) {
+                                var item = Assembly.LoadFile(file.FullName.Replace(".mod", ".dll"));
+                                foreach (var module in item.GetModules()) {
+                                    if (module.GetType() is T) {
+                                        return (T)Activator.CreateInstance(module.GetType());
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            return default(T);
         }
     }
 }
